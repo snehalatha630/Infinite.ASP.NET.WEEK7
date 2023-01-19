a53aa7a5e27438abd8e9e49225660f0564072eec
snehalatha630 <122988497+snehalatha630@users.noreply.github.com>
2023-01-19 11:05:40 +0530

    Add files via upload

---
+++ week7sql.sql
@@ -0,0 +1,9 @@
+﻿Alter procedure InsertPositionDetails
+    @cPositionCode char(4), @vDescription varchar(35),@iBudgetedStrength int, @siYear smallint,@iCurrentStrength int
+as
+begin
+    Insert Position (cPositionCode, vDescription,iBudgetedStrength, siYear,iCurrentStrength) values 
+    (@cPositionCode, @vDescription,@iBudgetedStrength, @siYear,@iCurrentStrength)
+end
+select*from Position
+exec InsertPositionDetails 'p001','Description Example','90','2023','92'
